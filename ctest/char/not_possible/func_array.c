5b06ccef64cf2a5a9857295535455184dfe48d07
abinayamg <46120363+abinayamg@users.noreply.github.com>
2019-05-17 12:22:25 +0530

    abi editted for dpendency check

--- ctest/char/not_possible/func_array.c
+++ ctest/char/not_possible/func_array.c
@@ -2,19 +2,24 @@
 
 #include<stdio.h>
 
-char add(char a[6], char b[6])
+/*char add(char a[6], char b[6])
 {
 char k[10] = {'w','i','n'};
 printf("func1= %s\n",a[6]);
 printf("func2= %s\n",b[6]);
 return k[1];
 }
+*/
 
 void main()
 {
-char x[6]="hey", y[6]="hello";
+  int a =10 , b=20;
+  int c = a+b;
+  printf("%d\n",c);
+/*char x[6]="hey", y[6]="hello";
 
 char v[1] = add(x[6],y[6]); // this is not allowed, getting in the function the whole array arguments 
 //int z[10] = sub(x,y);
 printf("sum: %s\n",v);
+*/
 }
